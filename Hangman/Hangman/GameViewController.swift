7f0880529aa8cb98406485439cb1077f1904bd31
Young Joong Kim <yjkimjunior@berkeley.edu>
2016-03-10 16:24:18 -0800

    part 1 specs met

--- Hangman/Hangman/GameViewController.swift
+++ Hangman/Hangman/GameViewController.swift
@@ -9,14 +9,90 @@
 import UIKit
 
 class GameViewController: UIViewController {
+    var wrongGuesses:[String] = []
+    var correctGuesses:[String] = []
+    var blanksOfAnswerArray:[String] = []
+    var globalPhrase:[String] = []
+    var counter = 1
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var blanksOfAnswer: UILabel!
+    @IBOutlet weak var userGuess: UITextField!
+   
+    @IBOutlet weak var wrongGuess: UIButton!
+    @IBOutlet weak var correctGuess: UIButton!
+    
+    @IBOutlet weak var listOfWrongGuesses: UILabel!
+    
+    func isGuessInPhrase(guess: String, phrase: [String]) -> Bool {
+        for letter in phrase {
+            if letter.lowercaseString == guess.lowercaseString {
+                return true
+            }
+        }
+        return false
+    }
+    
+    @IBAction func guessButton(sender: AnyObject) {
+        if isGuessInPhrase(userGuess.text!, phrase: globalPhrase) {
+            correctGuesses.append(userGuess.text!)
+            correctGuess.hidden = false
+        } else {
+            wrongGuesses.append(String(userGuess.text!))
+            wrongGuess.hidden = false
+        }
+        userGuess.text = ""
+    }
+    
+    //pressed wrong! button
+    @IBAction func wrongGuess(sender: AnyObject) {
+        counter++
+        if counter <= 7 {
+            imageView.image = UIImage(named: "hangman\(counter).gif")
+        }
+        else {
+            imageView.image = UIImage(named: "you-lose.gif")
+        }
+        
+        
+        listOfWrongGuesses.text = String(wrongGuesses)
+        wrongGuess.hidden = true
+    }
 
+    @IBAction func correctGuess(sender: AnyObject) {
+        //For Now: Just remove one blank
+        if blanksOfAnswerArray.count == 0 {
+            imageView.image = UIImage(named: "you-win.gif")
+        } else {
+            blanksOfAnswerArray.removeAtIndex(0)
+        }
+        blanksOfAnswer.text = blanksOfAnswerArray.joinWithSeparator("  ")
+        correctGuess.hidden = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        wrongGuess.hidden = true
+        correctGuess.hidden = true
+        
+//        if userGuess.text!.characters.count != 1 {
+//            print("wtf man")
+//        }
+        listOfWrongGuesses.text = wrongGuesses.joinWithSeparator(", ")
         let hangmanPhrases = HangmanPhrases()
-        var phrase = hangmanPhrases.getRandomPhrase()
+        let phrase = hangmanPhrases.getRandomPhrase()
         print(phrase)
+        for char in phrase.characters {
+            let ch = String(char)
+            globalPhrase.append(ch)
+        }
+        for var i = 0; i <= phrase.characters.count; i++ {
+            blanksOfAnswerArray.append("_")
+        }
+        blanksOfAnswer.text = blanksOfAnswerArray.joinWithSeparator("  ")
+        
     }
 
     override func didReceiveMemoryWarning() {
--- Hangman/Hangman/StartScreenViewController.swift
+++ Hangman/Hangman/StartScreenViewController.swift
@@ -32,4 +32,5 @@ class StartScreenViewController: UIViewController {
     }
     */
 
+
 }
